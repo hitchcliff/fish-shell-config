ffbd90f8ee57fa25e9e467cf86b48e778cbae0c3
Kevin Nacario <44961093+hitchcliff@users.noreply.github.com>
2022-12-15 16:19:58 +0800

    Update config.fish

--- config.fish
+++ config.fish
@@ -5,7 +5,7 @@ end
 function shorten_dir
 
 set -g fish_prompt_pwd_dir_length 1
-set -l realhome ~
+set realhome ~
 
 set_color 212922
 echo $PWD | sed -e "s|^$realhome|~|" -re 's-([^/.]{'"$fish_prompt_pwd_dir_length"'})[^/]*/-\1/-g';
@@ -18,8 +18,14 @@ echo (string split -r -m1 (basename $PWD) (shorten_dir))[1]
 
 end
 
+function color_user
+
+echo (set_color e47200 --reverse --bold) (user) (set_color normal); 
+
+end
+
 function fish_prompt
 
-echo (strip_basename)(set_color C3DF47)(basename $PWD)(set_color 294936) on(set_color 2BC)(git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/') '🐟: ';
+echo (color_user) (strip_basename)(set_color C3DF47)(basename $PWD)(set_color 294936) on(set_color 2BC)(git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/')(set_color ffffff) '❌ ';
 
 end
